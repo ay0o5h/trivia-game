0b6f1dae76aa0aed8d33bf0106be1516bc010111
ay0o5h <aayosh553@gmail.com>
2023-07-05 00:42:55 +0300

    add space vertical 20

---
+++ app/src/main/java/com/trivia/ui/composable/SpacerVertical20.kt
@@ -0,0 +1,13 @@
+package com.trivia.ui.composable
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.trivia.ui.theme.space_12
+import com.trivia.ui.theme.space_20
+
+@Composable
+fun SpacerVertical20(){
+    Spacer(modifier = Modifier.height(space_20))
+}
