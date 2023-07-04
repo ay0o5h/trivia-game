9b6518094f18e04bf793f511c1bac07df4b04621
shehab <sheshox12@gmail.com>
2023-07-04 18:19:37 +0300

    update branch

--- app/src/main/java/com/trivia/ui/composable/ButtonNext.kt
+++ app/src/main/java/com/trivia/ui/composable/ButtonNext.kt
@@ -12,26 +12,31 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.trivia.elements.IconArrow
-import com.trivia.ui.theme.Blue
-import com.trivia.ui.theme.Purple
+import com.trivia.ui.theme.PurpleDark
+import com.trivia.ui.theme.White36
 
 @Composable
 fun FilledCircularButton(isVisible: Boolean, modifier: Modifier = Modifier) {
-    val gradient = Brush.horizontalGradient(
-        colors = listOf(Purple, Blue)
+    val gradient = Brush.linearGradient(
+        colors = listOf(White36, PurpleDark),
+        start = Offset(x = -50f, y = 150f),
+        end = Offset(x = 250f, y = 0f),
     )
+
     AnimatedVisibility(visible = isVisible, enter = fadeIn(), exit = fadeOut()) {
-        Box(modifier = modifier
-            .size(56.dp)
-            .clip(shape = CircleShape)
-            .background(brush = gradient)
-            .clickable(interactionSource = CreateMutableInteractionSource(),
-                indication = CreateIndication(),
-                onClick = {})) {
+        Box(
+            modifier = modifier
+                .size(56.dp)
+                .clip(shape = CircleShape)
+                .background(brush = gradient)
+                .clickable(interactionSource = CreateMutableInteractionSource(),
+                    indication = CreateIndication(),
+                    onClick = {})
+        ) {
             IconArrow(
                 modifier = Modifier.align(Alignment.Center)
             )
--- app/src/main/java/com/trivia/ui/composable/IconArrow.kt
+++ app/src/main/java/com/trivia/ui/composable/IconArrow.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.trivia.R
 import com.trivia.ui.theme.White_70
+import com.trivia.ui.theme.White_87
 
 @Composable
 fun IconArrow(
@@ -17,7 +18,7 @@ fun IconArrow(
     Icon(
         painter = painterResource(id = R.drawable.arrowright),
         contentDescription = "arrowright",
-        tint = White_70,
+        tint = White_87,
         modifier = modifier.size(24.dp)
     )
 }
