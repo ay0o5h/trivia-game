27f8459e7b2c19ceb428e0e086c295b83588d41a
ay0o5h <aayosh553@gmail.com>
2023-07-06 00:55:56 +0300

    add trivia nav graph to main activity

--- app/src/main/java/com/trivia/MainActivity.kt
+++ app/src/main/java/com/trivia/MainActivity.kt
@@ -6,13 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import com.trivia.ui.screens.ResultScreen
-import com.trivia.ui.screens.SplashScreen
 import com.trivia.ui.theme.TriviaTheme
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +23,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ResultScreen()
+                    val navController = rememberNavController()
+                    TriviaNavGraph(navController = navController)
                 }
             }
         }
