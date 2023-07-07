df39b89b422383b3cc05f96f4abe0b600242c590
shehab <sheshox12@gmail.com>
2023-07-07 19:33:32 +0300

    update category screen

--- app/src/main/java/com/trivia/ui/screens/CategoryScreen.kt
+++ app/src/main/java/com/trivia/ui/screens/CategoryScreen.kt
@@ -1,6 +1,5 @@
 package com.trivia.ui.screens
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -14,8 +13,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.blur
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -23,8 +20,10 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.trivia.R
-import com.trivia.ScreensRoute
+import com.trivia.navigation.navigateToDifficultyScreen
 import com.trivia.ui.composable.ButtonNext
+import com.trivia.ui.composable.ImageBackground
+import com.trivia.ui.composable.ImagesScreenDecor
 import com.trivia.ui.composable.PrimaryButton
 import com.trivia.ui.theme.Typography
 import com.trivia.ui.theme.White_87
@@ -35,31 +34,30 @@ import com.trivia.viewmodel.state.CategoryUIState
 
 @Composable
 fun CategoryScreen(
-    navController: NavHostController,
-    viewModel: CategoryViewModel = hiltViewModel()
+    navController: NavHostController, viewModel: CategoryViewModel = hiltViewModel()
 ) {
-    val state by viewModel.stateCategory.collectAsState()
-
-    CategoryContent(state, viewModel, navController)
+    val state by viewModel.state.collectAsState()
+    fun navigateToDifficultyScreen() {
+        navController.navigateToDifficultyScreen(state.selectedCategory)
+    }
+    CategoryContent(state, viewModel, ::navigateToDifficultyScreen)
 }
 
 
 @Composable
 fun CategoryContent(
-    state: CategoryUIState,
-    viewModel: CategoryScreenInteractions,
-    navController: NavHostController
+    state: CategoryUIState, viewModel: CategoryScreenInteractions, onNavigate: () -> Unit
 ) {
     Box(
         modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
     ) {
-        Image(
-            modifier = Modifier
-                .fillMaxSize()
-                .blur(40.dp),
-            painter = painterResource(id = R.drawable.background),
-            contentScale = ContentScale.FillBounds,
-            contentDescription = "background image"
+
+        ImageBackground()
+
+        ImagesScreenDecor(
+            header = painterResource(id = R.drawable.group_astrounat), footer = painterResource(
+                id = R.drawable.group_space
+            )
         )
 
         Column(
@@ -90,7 +88,7 @@ fun CategoryContent(
             ButtonNext(
                 state.isButtonNextVisible,
                 modifier = Modifier.padding(top = 48.dp),
-                onClick = { navController.navigate(ScreensRoute.Difficulty.route) }
+                onClick = onNavigate
             )
 
         }
